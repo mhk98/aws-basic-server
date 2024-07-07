74b14707cf8b377668e57d579c79c9acf583811d
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2024-07-07 12:05:21 +0600

    server 2

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World from server 1')
+  res.send('Hello World from server 2')
 })
 
 app.listen(port, () => {
-  console.log(`Example app 1 listening on port ${port}`)
+  console.log(`Example app 2 listening on port ${port}`)
 })
