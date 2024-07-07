cf8281449037d6eeee576fd90bc6c00867972cf8
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2024-07-07 11:42:46 +0600

    latest

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World from aws & remove port and altime project live using nginx!')
+  res.send('Hello World from server 1')
 })
 
 app.listen(port, () => {
